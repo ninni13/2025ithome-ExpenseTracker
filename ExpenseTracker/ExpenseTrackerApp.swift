d9a227b0eb3522bd2451d5a1a7f4baae88f85528
Nini <94174963+ninni13@users.noreply.github.com>
2025-08-27 19:39:15 +1000

    Initial Commit

---
+++ ExpenseTracker/ExpenseTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseTrackerApp.swift
+//  ExpenseTracker
+//
+//  Created by Nini on 2025/8/27.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ExpenseTrackerTests/ExpenseTrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseTrackerTests.swift
+//  ExpenseTrackerTests
+//
+//  Created by Nini on 2025/8/27.
+//
+
+import Testing
+@testable import ExpenseTracker
+
+struct ExpenseTrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
